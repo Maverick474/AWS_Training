fcc893ce23264b013ac1913d1f89ed9963114c43
zainH474 <142337757+zainH474@users.noreply.github.com>
2024-07-31 16:56:10 +0500

    Lesson2
    covered basics of Node js

---
+++ Node js (basics)/app.js
@@ -0,0 +1,74 @@
+const http = require('http') //we use require to specify a path and import files/module to js file
+const fs = require('fs') // for importing file system core module
+const { buffer } = require('stream/consumers')
+
+// define the function once use createServer()
+
+/*
+function rqListener(request, response) {
+
+}
+*/
+
+//Alternativly we can create an anymous functions
+/*
+
+http.createServer((req, res) => {
+    console.log(req)
+}) //The createserver takes two arguments one for request listener other for response
+
+*/
+
+
+const server = http.createServer((req, res) => {
+    console.log(req)
+})
+
+server.listen(3000)
+
+// The above code is stuck inside the event loop means it will run contionously however once we use process.exit() it will break from the loop
+
+
+// we can also return the specific url and method by using these functions
+
+const server2 = http.createServer((req, res)=>{
+    const url = req.url
+    const method = req.method
+    if (url === '/') {
+
+        res.write('<html>')
+        res.write('<head><title>Node js</title></head>')
+        res.write('<body><form method="POST" action="/message" ><input type="text" name="message><button type="submit">Submit</button></input></form><body>')
+        res.write('</html>')
+        return res.end() // the return res.end() prevents the other block of code intefering or causing error
+    }
+    if (url==='/message' && method === 'POST') {
+        const body = []
+        req.on('data', (chunk)=>{
+            body.push(chunk)
+        });
+        req.on('end', ()=>{
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            //Notice we have used sync instead of create file function because it will only execute
+            //Other lines of code when this line (55) is completed
+            fs.writeFileSync('message.txt', message) // a messagetxt file will be created and a dummy text will be inserted
+            res.statusCode = 302;
+            res.setHeader('Location', '/')
+            return res.end()
+        })
+
+    }
+    res.setHeader('Content-Type', 'text/html') // specifying the web document type
+    res.write('<html>')
+    res.write('<head><title>Node js</title></head>')
+    res.write('<body><h1>Hi from Node js server <h1/><body>')
+    res.write('</html>')
+    res.end() // specifying the end the above code will not work if this isn't specified
+
+})
+
+server2.listen(4000)
+// req.url returns specific url req.header return the top header info req.method returns which method is used,
+// like post get etc.
+
