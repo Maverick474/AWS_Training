f574cf1e4c0b3ae4cec65aa3b40fa9908f1a93ae
Muhammad Zain haseeb <kzain105@gmail.com>
2024-08-07 17:25:01 +0500

    SQL introduction

---
+++ introduction/index.js
@@ -0,0 +1,26 @@
+/*
+
+MongoDB:
+
+i) it is a no-sql based database (works on documents)
+
+ii) strong support for Aggeration pipes
+
+iii) works on BSON format
+
+iv) Best for Node js compability
+
+Commands for MongoDB:
+
+i) show dbs 
+
+ii) use <db_name>
+
+iii) show collections
+
+iv) db.collection.insert({ }) insert key value pairs
+
+v) db.collection.find({})
+
+
+*/
